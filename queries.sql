7b6599a3d98ca7b7af647e0e2680b7c2668367d8
nikkisuneel <53950214+nikkisuneel@users.noreply.github.com>
2023-06-04 23:20:02 -0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,7 @@
+# Nikki's Queries
+# Query 1: Combines the averages of the total spending for each category of 
+# products and groups them by the levels of education of the customers:
 
+
+# Query 2: analyzing the if and what the relationship is between customers and 
+# their varying income levels with how they respond to varying marketing campaigns.
