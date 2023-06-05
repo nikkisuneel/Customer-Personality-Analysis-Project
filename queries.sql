e52e98f6bf04132c4d925f141a0e050f68f0a538
nikkisuneel <53950214+nikkisuneel@users.noreply.github.com>
2023-06-04 23:20:21 -0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,7 +1,11 @@
 # Nikki's Queries
 # Query 1: Combines the averages of the total spending for each category of 
 # products and groups them by the levels of education of the customers:
+SELECT c.education, pt.name AS category, AVG(cp.amount_spent) AS average_spending
+FROM Customer AS c
+JOIN Customer_Product_Type AS cp ON c.id = cp.customer_id
+JOIN Product_Type AS pt ON cp.product_id = pt.id
+GROUP BY c.education, pt.name;
 
-
-# Query 2: analyzing the if and what the relationship is between customers and 
+# Query 2: Analyzing the if and what the relationship is between customers and 
 # their varying income levels with how they respond to varying marketing campaigns.
